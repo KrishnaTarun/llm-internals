0ded6c59a83d249c634431ff1efdea72c6577108
Tarun Krisha <krishnatarun7@gmail.com>
2026-08-30 23:42:41 +0100

    2. Add multihead attention (requires some fixes)

--- src/llm_internals/attention.py
+++ src/llm_internals/attention.py
@@ -28,6 +28,8 @@ class Attention(nn.Module):
     def __init__(self, dmodel:torch.Tensor, num_heads:int, dropout:float=0.0) -> None:
         super().__init__()
 
+        #TODO: add dropout and masking functionality
+
         #this is embedding dimension
         self.emb_dim = dmodel
         self.heads = num_heads
@@ -36,13 +38,17 @@ class Attention(nn.Module):
         assert self.emb_dim % self.heads == 0, "Embedding dimension must be divisible by number of heads"
 
         #projection heads, W(q, i), W(k, i), W(v, i) for each head i.
+        """
+        To avoid confusion W_q, w_k, W_v could have been also intialized with 
+        nn.Linear(emb_dim, emd_dim), emb_dim = head_dim * heads, but we are intializing 
+        them with nn.Linear(emb_dim, head_dim * heads).
+        """
+
+        self.W_Q = nn.Linear(self.emb_dim, self.head_dim * self.heads, bias=False) 
+        self.W_K = nn.Linear(self.emb_dim, self.head_dim * self.heads, bias=False)
+        self.W_V = nn.Linear(self.emb_dim, self.head_dim * self.heads, bias=False)
         
-
-        #intializw W(O) projection matrix
-        self.W_O = nn.Linear(self.head_dim * self.heads, self.emb_dim) 
-
-
-
+        self.W_O = nn.Linear(self.head_dim * self.heads, self.emb_dim, bias=False) 
 
 
     def _scaled_dot_product(self, query, key, value, mask=None):
@@ -65,10 +71,27 @@ class Attention(nn.Module):
 
 
     def forward(self, query, key, value):
-        return(self._scaled_dot_product(query, key, value))
 
 
-mod = Attention(dim=8)
+        q = self.W_Q(query) # (b, seq_len, emb_dim)
+        k = self.W_K(key) # (b, seq_len, emb_dim)
+        v = self.W_V(value) # (b, seq_len, emb_dim)
+
+        # prepare q, k, v for multi-head attention
+        q = q.view(q.size(0), q.size(1), self.heads, self.head_dim).transpose(1, 2)  # (b, heads, seq_len, head_dim)
+        k = k.view(k.size(0), k.size(1), self.heads, self.head_dim).transpose(1, 2)  # (b, heads, seq_len, head_dim)
+        v = v.view(v.size(0), v.size(1), self.heads, self.head_dim).transpose(1, 2)  # (b, heads, seq_len, head_dim)
+
+        # reshape it to (b, seq_len, heads * head_dim)
+        at_heads = self._scaled_dot_product(q, k, v)  # (b, heads, seq_len, head_dim)
+        at_heads = at_heads.transpose(1, 2).contiguous() # (b, seq_len, heads, head_dim)
+        at_heads = at_heads.view(at_heads.size(0), -1, self.emb_dim)  # (b, seq_len, heads * head_dim)
+
+        return self.W_O(at_heads)  # (b, seq_len, emb_dim)
+
+
+
+mod = Attention(dmodel=8, num_heads=2, dropout=0.1)
 out = mod(torch.randn(2, 4, 8), torch.randn(2, 4, 8), torch.randn(2, 4, 8))  
 print(out.shape)  
 
