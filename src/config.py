efc12a26ef3cbcf33a9d785f71a7e9e65caa5830
Tarun Krishna <KrishnaTarun@users.noreply.github.com>
2026-09-10 22:24:11 +0100

    Dataset/setup translation data (#6)

---
+++ src/config.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass, field
+
+import torch
+import dacite
+import yaml
+
+@dataclass
+class ModelConfig:
+    model_type: str = "Seq2Seq"
+    d_model: int = 128
+    dff: int = 512
+    num_heads: int = 8
+    enc_num_layers: int = 2
+    dec_num_layers: int = 1
+    dropout: float = 0.1
+
+@dataclass
+class DatasetConfig:
+
+    tokenizer_path: str = "translation_en-nl_tokenizer.json"
+    seq_len: int = 100
+    src_vocab_size: int = 100
+    tgt_vocab_size: int = 100
+    src_pad_idx: int = 0
+    tgt_pad_idx: int = 0
+
+@dataclass
+class TrainingConfig:
+
+    device: str = field(default_factory=lambda: "cuda" if torch.cuda.is_available() else "cpu")
+    batch_size: int = 32
+    num_epochs: int = 10
+    learning_rate: float = 0.001
+    weight_decay: float = 0.0001
+    warmup_steps: int = 4000
+    seed: int = 42
+
+@dataclass
+class Config:
+    
+    model: ModelConfig = field(default_factory=ModelConfig)
+    training: TrainingConfig = field(default_factory=TrainingConfig)
+    data: DatasetConfig = field(default_factory=DatasetConfig)
+
+def load_config(path: str) -> Config:
+    with open(path, "r", encoding="utf-8") as f:
+        config_data = yaml.safe_load(f)
+    return dacite.from_dict(Config, config_data)
---
+++ src/train.py
@@ -0,0 +1,39 @@
+import torch
+from torch import nn
+
+from config import Config, load_config
+from model import Seq2SeqModel
+
+
+#TODO: GPU check and device assignment
+def train(config:Config):
+    """
+    Train the model based on the provided configuration.
+
+    Args:
+        config (Config): Configuration object containing model, training, and dataset parameters.
+    """
+    # Set random seed for reproducibility
+    torch.manual_seed(config.training.seed)
+
+    # Initialize the model
+    #FIXME: Add support for other model types in the future
+    if config.model.model_type == "Seq2Seq":
+        model = Seq2SeqModel(config).to(config.training.device)
+
+    # Define loss function and optimizer
+    criterion = nn.CrossEntropyLoss(ignore_index=config.data.tgt_pad_idx)
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.training.learning_rate, weight_decay=config.training.weight_decay)
+
+    # Placeholder for data loading (to be implemented)
+    # train_loader = ...
+
+    
+
+if __name__ == "__main__":
+    # Load configuration from YAML file
+    config = load_config("configs/seq2seq_training.yaml")
+    print(config)
+
+    # Start training
+    train(config)
