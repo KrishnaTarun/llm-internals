85e4bd7f206e04724ea4492e339db6162b90663b
Tarun Krishna <KrishnaTarun@users.noreply.github.com>
2026-09-13 22:26:43 +0100

    Feature/training pipeline (#7)

--- src/train.py
+++ src/train.py
@@ -3,6 +3,8 @@ from torch import nn
 
 from config import Config, load_config
 from model import Seq2SeqModel
+from utils import get_model, get_dataset
+from trainer import LLMTrainer
 
 
 #TODO: GPU check and device assignment
@@ -14,19 +16,25 @@ def train(config:Config):
         config (Config): Configuration object containing model, training, and dataset parameters.
     """
     # Set random seed for reproducibility
+    # FIXME this properly
     torch.manual_seed(config.training.seed)
+
+    #get data
+    #FIXME
+    dataset, train_loader, val_loader = get_dataset(config)
+    src_pad_id = dataset.src_pad_id
+    tgt_pad_id = dataset.tgt_pad_id
+    src_vocab_size = dataset.src_vocab_size
+    tgt_vocab_size = dataset.tgt_vocab_size
 
     # Initialize the model
     #FIXME: Add support for other model types in the future
-    if config.model.model_type == "Seq2Seq":
-        model = Seq2SeqModel(config).to(config.training.device)
+    model = get_model(config, src_vocab=src_vocab_size, tgt_vocab= tgt_vocab_size)
 
-    # Define loss function and optimizer
-    criterion = nn.CrossEntropyLoss(ignore_index=config.data.tgt_pad_idx)
-    optimizer = torch.optim.Adam(model.parameters(), lr=config.training.learning_rate, weight_decay=config.training.weight_decay)
 
-    # Placeholder for data loading (to be implemented)
-    # train_loader = ...
+    trainer = LLMTrainer(model, config, dataset)
+    trainer.fit(train_loader, val_loader)
+
 
     
 
--- src/trainer.py
+++ src/trainer.py
@@ -1,19 +1,32 @@
 
+import numpy as np
+
 import torch
 from torch import nn
+from torch.utils.data import Dataset
 
 from config import Config
 
-class Trainer:
-    def __init__(self, model: nn.Module, config: Config):
-        self.model = model
-        self.criterion = nn.CrossEntropyLoss(ignore_index=config.data.tgt_pad_idx)
-        self.optimizer = torch.optim.Adam(model.parameters(), lr=config.training.learning_rate, weight_decay=config.training.weight_decay)
+class LLMTrainer:
+    def __init__(self, model: nn.Module, config: Config, dataset: Dataset):
+
+        
+        self.config = config
+        self.model = model.to(config.training.device)
+        self.device = config.training.device 
+        self.dataset = dataset
+
+
+        self.criterion = nn.CrossEntropyLoss(ignore_index=self.dataset.tgt_pad_id)
+        self.optimizer = torch.optim.Adam(model.parameters(), 
+                                          lr=config.training.learning_rate,
+                                          weight_decay=config.training.weight_decay)
+        
         self.device = config.training.device
 
-    def _train_step(self, src_batch, tgt_batch):
+    def _train_epoch(self, loader):
         """
-        Perform a single training step.
+        Perform a single epoch fo training.
 
         Args:
             src_batch (torch.Tensor): Source batch of shape (batch_size, seq_len).
@@ -23,21 +36,30 @@ class Trainer:
             float: The loss value for the current training step.
         """
         self.model.train()
-        src_batch = src_batch.to(self.device)
-        tgt_batch = tgt_batch.to(self.device)
+        total_loss = 0.0
+        
 
-        # Forward pass
-        output = self.model(src_batch, tgt_batch[:, :-1])  # Exclude the last token for teacher forcing
-        loss = self.criterion(output.reshape(-1, output.size(-1)), tgt_batch[:, 1:].reshape(-1))  # Shift target for loss calculation
+        for batch in loader:
+            #FIXME write assertions
+            src_batch = batch["src"].to(self.device)
+            tgt_batch = batch["tgt"].to(self.device)
+            label = batch["label"].to(self.device)
+            padding_mask = batch["src_mask"].to(self.device)
+            causal_mask = batch["causal_mask"].to(self.device)
 
-        # Backward pass and optimization
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
+            logits = self.model(src_batch, tgt_batch, causal_mask, padding_mask)
+            loss = self.criterion(logits.view(-1, self.dataset.tgt_vocab_size), label.view(-1))
+            total_loss+=loss.item()
 
-        return loss.item()
+            # Backward pass and optimization
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
 
-    def fit(self, train_loader, num_epochs):
+        return total_loss / len(loader)
+
+    def fit(self, train_loader, val_loader):
+        
         """
         Train the model for a specified number of epochs.
 
@@ -45,14 +67,105 @@ class Trainer:
             train_loader (torch.utils.data.DataLoader): DataLoader for training data.
             num_epochs (int): Number of epochs to train the model.
         """
+        #TODO: Average  meter 
+        num_epochs = self.config.training.num_epochs
         for epoch in range(num_epochs):
-            total_loss = 0
-            for src_batch, tgt_batch in train_loader:
-                loss = self._train_step(src_batch, tgt_batch)
-                total_loss += loss
 
-            avg_loss = total_loss / len(train_loader)
-            print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {avg_loss:.4f}")
+            train_loss = self._train_epoch(train_loader)
+            self.greedy_decode(val_loader)
+            # validation_loss = self.evaluate(val_loader)
+            print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {train_loss:.4f}")
+            # print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {validation_loss:.4f}")
+
+        #TODO save model
+            
             
     @torch.no_grad()
     def evaluate(self, val_loader):
+        self.model.eval()
+        val_loss = 0.0
+
+        for batch in val_loader:
+            src_batch = batch["src"].to(self.device)
+            tgt_batch = batch["tgt"].to(self.device)
+            label = batch["label"].to(self.device)
+            padding_mask = batch["src_mask"].to(self.device)
+            causal_mask = batch["causal_mask"].to(self.device)
+
+            logits = self.model(src_batch, tgt_batch, causal_mask, padding_mask)
+            val_loss += self.criterion(logits.view(-1, self.dataset.tgt_vocab_size), label.view(-1)).item()
+        return val_loss/ len(val_loader)
+
+    
+    @torch.no_grad()
+    def greedy_decode(self,loader)->None:
+
+        """
+        Loader here is basically a test_loader or validation loader.
+        This could be also modified for taking a sentence but its a TODO.
+        """
+
+        self.model.eval()
+        #generate random sentence ids to verify
+        sentence_ids = np.random.randint(0, len(loader.dataset), size=(2,))
+        
+        for sample_id in sentence_ids:
+            sentence = loader.dataset[int(sample_id)]
+            #Add a extra dimension for batch
+            src = sentence["src"].to(self.device).unsqueeze(0)
+            src_mask = sentence["src_mask"].to(self.device).unsqueeze(0)
+
+            #get encoder out
+            enc_out = self.model.encoderblock(src, src_mask) # (b=1, seq_len, emb_dim)
+
+            dec_in = torch.empty(1,1).fill_(self.dataset.tgt_tokenizer.start_token_id).type_as(src).to(self.device)
+            while True:
+
+                if dec_in.size(1) == self.config.data.seq_len:
+                   self.idstotext(src, sentence["tgt"], dec_in.squeeze(0))
+                   break
+                causal_mask= torch.tril(torch.ones((dec_in.size(1), dec_in.size(1)))).unsqueeze(0)
+                #unsqeeze extra to add batch diemsnion
+                causal_mask = causal_mask.unsqueeze(0)
+
+                dec_out = self.model.decoderblock(enc_out, dec_in, causal_mask, src_mask)
+                prob = self.model.projection_layer(dec_out[:, -1]) #select the recently generated output
+                _, next_word = torch.max(prob, dim=1)
+                #FIXME this
+                dec_in = torch.cat([dec_in, torch.empty(1, 1).type_as(src).fill_(next_word.item()).to(self.device)], dim=1)
+
+                if next_word == self.dataset.tgt_tokenizer.end_token_id:
+                    #decode and break
+                    self.idstotext(src, sentence["tgt"], dec_in.squeeze(0)) # get rid of batch dimension
+                    
+
+                    break
+
+        return
+
+    def idstotext(self, src, tgt, gen_ids)->None:
+
+        src_text = self.dataset.src_tokenizer.tokenizer.decode(src.squeeze(0).cpu().numpy().tolist(), skip_special_tokens=True)
+        tgt_text = self.dataset.tgt_tokenizer.tokenizer.decode(tgt.cpu().numpy().tolist(), skip_special_tokens=True)
+        gen_text = self.dataset.tgt_tokenizer.tokenizer.decode(gen_ids.cpu().numpy().tolist(), skip_special_tokens=True)
+        print()
+        print(f"Input Soruce text =====> {src_text}")
+        print(f"===================================")
+        print(f"Target text ===========> {tgt_text}")
+        print(f"===================================")
+        print(f"Generated text ========> {gen_text}")
+        print()
+
+        return
+
+
+
+
+
+            # tgt_sentence = sentence["tgt"].to(self.device)
+            # label = sentence["label"].to(self.device)
+            # causal_mask = sentence["causal_mask"].to(self.device) 
+            
+
+
+        
